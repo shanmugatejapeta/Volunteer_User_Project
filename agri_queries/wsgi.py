705002159275ceaa9db4580b260e07d59addd58f
shanmugatejapeta <128489302+shanmugatejapeta@users.noreply.github.com>
2024-06-16 22:26:41 +0530

    Update wsgi.py

--- agri_queries/wsgi.py
+++ agri_queries/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agri_queries.settings')
 
 application = get_wsgi_application()
+
+app=application
